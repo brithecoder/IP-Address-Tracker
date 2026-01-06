d4489a04e8214ded0f12286833e5df26bbad999d
Brianna Gordon <brigordon574@gmail.com>
2026-01-06 11:21:52 -0500

    inital commit of code

---
+++ ip-address-tracker/src/App.tsx
@@ -0,0 +1,14 @@
+import AppContainer from './Components/AppContainer'
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div>
+    <AppContainer />
+   </div>
+  )
+}
+
+export default App
---
+++ ip-address-tracker/src/Components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Searchbar from './Searchbar'
+
+export default function Navbar() {
+  return (
+    <div>
+        <h1>IP Address Tracker</h1>
+        <Searchbar />
+    </div>
+  )
+}
